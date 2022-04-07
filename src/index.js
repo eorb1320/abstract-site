82a6a10008c81c42e4d20bb5dccd6c59a1d5b771
eorb1320 <eorb910623@gmail.com>
2022-04-07 21:04:36 +0900

    테스트코드

--- src/index.js
+++ src/index.js
@@ -1,11 +1,127 @@
-class Site {}
 
-class Board {}
+/*
+class Site{} 상태로 클래스가 만들어져 있어도 index.spec.js에서 객체 선언해도 상관없음 에러는 안남
+하지만 객체를 생성할때 어떠한 파라미터을 넘겨도 Site {} 상태로 만들어짐
+그래서 특정한 값들로 객체를 구성해주는 constructor(){} 생성
+객체를 구성하는 값을 세팅해주는 함수(정확한 개념은 다시한번 찾아봐야할듯)
+//
+*/
+const boardList = [];
+const articleList =[];
+class Site {
+    constructor() {  
+        this.boards = []; 
+    }
 
-class Article {}
+    /*
+        Class Site 객체가 생성될때 만들어진 this.boards 라는 배열에
+        파라미터로 넘어온 board를 push 해주는 함수
+    */
+    addBoard(board) {
+        /**
+        find 함수 : 배열에 입력한 조건에 해당하는 행을 반환
+        this.boards 에 담겨 있는 데이터의 boardName을 체크 해서 
+        넘어온 board 의 boardName과 일치하는 데이터가 존재하면 throw true 에러를 반환
+        존재하지 않는다면 this.board에 넘어온 board를 push
+        */
+        const findBoard = this.boards.find(item => item.boardName === board.boardName);
 
-class Comment {}
+        // for(var i = 0; i<this.boards.length;i++){
+        //     var item = this.boards[i];
+        //     if(item.boardName == board.boardName){
+        //         return item;
+        //     }
+        // }
 
+        if(findBoard==undefined){
+            this.boards.push(board);
+            boardList.push(board);
+        }else{
+            throw true;
+        }
+    }
+    /**
+     addBoard(board) 함수를 통하여 this.boards 배열에 push되어진
+     board 내용들 중에 boardName이 일치하는 board를 반환
+     */
+    findBoardByName(boardName){
+        //addBoard() 함수에서 사용된 find 함수를 이용하여 일치하는 boardName을 찾아서 반환
+        const findBoard = this.boards.find(item => item.boardName === boardName);
+        return findBoard;
+    }
+
+}
+
+class Board {
+    constructor(boardName){
+        if(emptyCheck(boardName)){
+            this.boardName = boardName; 
+            this.article=[];           
+        }else{
+            throw true;
+        }
+    }
+    publish(article){
+        const findBoard = boardList.find(item => item.boardName === this.boardName);
+        if(findBoard!=undefined){
+            this.article.push(article);
+            articleList.push(article);
+        }else{
+            throw true;
+        }
+        
+    }
+    getAllArticles(){
+        return this.article;
+    }
+} 
+
+class Article {
+    constructor(boardData){
+        if(!emptyCheck(boardData.subject) || !emptyCheck(boardData.subject) || !emptyCheck(boardData.subject)){
+            throw true;
+        }else{
+            this.id = boardList[boardList.length-1].boardName+'-'+(articleList.length+1);
+            this.boardData = boardData;
+            this.createdDate = new Date().toISOString();
+            this.comment=[];
+        }
+    }
+    reply(commentData){  
+        const findArticle = articleList.find(item => item.id === this.id);
+        if(findArticle!=undefined){
+            this.comment.push(commentData);
+        }else{
+            throw true;
+        }
+    }
+    getAllComments(){
+        return  this.comment;
+    }
+}
+
+class Comment {
+    constructor(commentData){
+        if(!emptyCheck(commentData.content) || !emptyCheck(commentData.author)){
+            throw true;
+        }else{
+            this.content = commentData.content;
+            this.author  = commentData.author;
+            this.createdDate = new Date().toISOString();
+        }
+
+    }
+}
+
+
+//공백,null,undefined  체크 함수
+function emptyCheck(data){
+    if(data == undefined || data ==null || data==""){
+        return false;
+    }else{
+        return true;
+    }
+}
 module.exports = {
     Site,
     Board,
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,64 +1,170 @@
+const ConfusingGlobals = require('confusing-browser-globals');
 const { Site, Board, Article, Comment } = require('.');
 
+
 describe('Site 요구사항 테스트', () => {
+
+    //Site 라는 객체가 생성되는지 테스트
+    //1. index.spec.js 1line  - const { Site, Board, Article, Comment } = require('.');
+    //2. index.js 9  line - Class Site {} 선언
+    //3. index.js         - module.exports= {Site} 선언
+    /* 위 3개 정상적으로 선언되어져서  
+    test('TEST문구' , ()=> {
+        //위치에서 const 변수이름 = new Site(); 객체가 선언되어도 에러가 나지 않는지 테스트
+    })
+    */
+   //1번 
     test('Site는 n개 이상 생성 할 수 있다.', () => {
         expect(() => {
+        // Site 객체 생성
+        // Class Site
+        /*
+            constructor 에
+            this.boards = []; 라고 선언되어져 있기 때문에
+            //결과
+            const _site1 = Site {boards:[]} ;
+            const _site2 = Site {boards:[]} ;
+            상태로 생성되며
+            정상적으로 생성이 되었기 때문에 에러가(throw) 가 발생하지 않으며
+            test는 정상적으로 pass
+        */
             const _site1 = new Site();
             const _site2 = new Site();
         }).not.toThrow();
     });
-
+    
+    //2번
     test('Site에는 Board를 추가하고 추가된 Board를 조회할 수 있다.', () => {
-        const mySite = new Site();
+        
+        // Site 객체 생성
+        // Class Site
+        /*
+            mySite = Site {boards :[]} ;
+        */
+        const mySite = new Site(); 
+        /*
+            Class Board 
+
+            constructor에는 this.boardName = boardName; 이라고 선언되어져 있기 때문에
+            매개변수(파라미터)로 넘어온 '공지사항' 의 값이
+            this.boardName = '공지사항'
+
+            const noticeBoard = Board { boardName: '공지사항' }
+
+            //const noticeBoard = new Board('공지사항',1,2);
+            //const noticeBoard = Board { boardName: '공지사항' , boardNum:1 , userNum:2 }
+            형태로 객체가 생성됨
+        */
         const noticeBoard = new Board('공지사항');
 
-        mySite.addBoard(noticeBoard);
+        /*
+            위에서 new Site() 를 통해서 만들어진 객체의 함수를 호출가능
+            index.js 14 line - addBoard(board){}
 
+            const noticeBoard = Board { boardName: '공지사항' }형태로 생성된 객체
+            noticeBoard를 mySite.addBoard() 에 파라미터로 넘김
+
+            mySite = Site {boards :[{ boardName: '공지사항' }]} ;
+            형태로 담김
+        */
+       // mySite = Site {boards :[]} ;
+        mySite.addBoard(noticeBoard);
+        // mySite = Site {boards :[{ boardName: '공지사항' }]} ;
+
+        //findBoardByName 함수를 이요해 boardName이 '공지사항' 일치하는 board를 찾아서 
+        //noticeBoard 와 일치하는지 확인 
+        //원하는 객체와 일치하는 객체가 존재하는지 확인후 패스
+
+        //mySite.findBoardByName('공지사항') = { boardName: '공지사항' }
+        //비교할 데이터 noticeBoard =  { boardName: '공지사항' }
         expect(mySite.findBoardByName('공지사항')).toEqual(noticeBoard);
     });
 
+    //3번
     test('하나의 Site에 동일한 이름의 Board를 추가할 수 없다.', () => {
         const mySite = new Site();
+        //const noticeBoard1 = Board { boardName: '공지사항' }
         const noticeBoard1 = new Board('공지사항');
+        //const noticeBoard2 = Board { boardName: '공지사항' }
         const noticeBoard2 = new Board('공지사항');
 
-        mySite.addBoard(noticeBoard1);
+        // mySite = Site {boards :[]} ;
+        mySite.addBoard(noticeBoard1); 
+        // mySite = Site {boards :[{ boardName: '공지사항' }]} ;
 
         expect(() => {
             mySite.addBoard(noticeBoard2);
         }).toThrow();
     });
 
+    //4번 
     test('Board는 n개 이상 추가 할 수 있다.', () => {
+        // mySite = Site {boards :[]} ;
         const mySite = new Site();
+        //const noticeBoard = Board { boardName: '공지사항' }
         const noticeBoard = new Board('공지사항');
+        //const faqBoard = Board { boardName: 'FAQ' }
         const faqBoard = new Board('FAQ');
 
         expect(() => {
+            // mySite = Site {boards :[]} ;
             mySite.addBoard(noticeBoard);
+            // mySite = Site {boards :[{ boardName: '공지사항' }]} ;
             mySite.addBoard(faqBoard);
+            // mySite = Site {boards :[{ boardName: '공지사항' }, { boardName: 'FAQ' }]} ;
         }).not.toThrow();
-
+        //mySite.boards = [{ boardName: '공지사항' }, { boardName: 'FAQ' }]
+        //[{ boardName: '공지사항' },  { boardName: 'FAQ' }]
         expect(mySite.boards).toEqual([noticeBoard, faqBoard]);
     });
 });
 
+
+//Borad에 Article 생성테스트
 describe('Board 요구사항 테스트', () => {
+    /*
+        describe('Board 요구사항 테스트', () => {
+            test() 에 속하지 않고 전역 변수로 let mySite 선언
+        });
+    */
     /**
      * @type {Site}
      */
-    let mySite;
 
+    let mySite;
+    
+    //전역 변수 객체 선언
+    //아래 테스트 진행 하며 초기화 되지 않고 계속 사용됨
     beforeEach(() => {
         // NOTE: Reset `mySite`
         mySite = new Site();
     });
-
+    //5번
     test('Board는 name 데이터를 포함해야 하며 null 또는 빈 문자열("")은 허용하지 않는다.', () => {
         expect(() => {
+             //const _board = Board { boardName: '공지사항' }
+             //공지사항이라는 값이 존재 하기때문에 객체 생성 성공
             const _board = new Board('공지사항');
         }).not.toThrow();
+        // 기존에 
+            /*
+                constructor(boardName){
+                    this.boardName = boardName; 
+                }
+                Class Board 에 공백,null 체크를 하지 않은 상태에서
+                '' , null 값을 boardName으로 넘겨도 문제는 안됨
+                하지만 공백,null로는 객체 생성을 막기 위해 
+                constructor 부분을 아래 와 같이수정
 
+                constructor(boardName){
+                    if(boardName!=="" && boardName!==null){
+                        this.boardName = boardName; 
+                        this.article=[];           
+                    }else{
+                        throw true;
+                    }
+                }
+            */
         expect(() => {
             const _board = new Board('');
         }).toThrow();
@@ -66,12 +172,14 @@ describe('Board 요구사항 테스트', () => {
         expect(() => {
             const _board = new Board(null);
         }).toThrow();
+        //constructor 수정후 '' , null 값이 넘어가면
+            // throw true; 가 return 되며 pass
     });
-
+    //6번
     test('Site 에 추가된 Board만 사용 가능한 것으로 간주하며 사용 불가능한 Board에는 Article을 추가할 수 없다.', () => {
         const addedBoard = new Board('사이트에 추가된 게시판');
         const notAddedBoard = new Board('사이트에 추가되지 않은 게시판');
-
+        
         mySite.addBoard(addedBoard);
 
         expect(() => {
@@ -82,7 +190,7 @@ describe('Board 요구사항 테스트', () => {
             });
             addedBoard.publish(article);
         }).not.toThrow();
-
+        
         expect(() => {
             const article = new Article({
                 subject: '글 제목2',
@@ -92,7 +200,7 @@ describe('Board 요구사항 테스트', () => {
             notAddedBoard.publish(article);
         }).toThrow();
     });
-
+    //7번
     test('Board에 Article을 추가할 때 Article에 ID를 자동 생성해서 부여해야 한다.', () => {
         const noticeBoard = new Board('공지사항');
         mySite.addBoard(noticeBoard);
@@ -103,11 +211,11 @@ describe('Board 요구사항 테스트', () => {
             author: '강승현',
         });
         noticeBoard.publish(article);
-
         // 규칙은 ${board.name}-${랜덤 값} 를 따른다.
         expect(article.id.startsWith('공지사항-')).toBe(true);
     });
 
+    //8번
     test('Board에 Article을 추가할 때 Article에 작성 일자가 들어가야 한다.', () => {
         const noticeBoard = new Board('공지사항');
         mySite.addBoard(noticeBoard);
@@ -118,11 +226,11 @@ describe('Board 요구사항 테스트', () => {
             author: '강승현',
         });
         noticeBoard.publish(article);
-
         // createdDate가 저장되는 형식은 ISO 8601을 따른다.
         expect(/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/.test(article.createdDate)).toBe(true);
     });
 
+    //9번
     test('Article 은 n개 이상 추가 할 수 있다.', () => {
         const noticeBoard = new Board('공지사항');
         mySite.addBoard(noticeBoard);
@@ -145,6 +253,8 @@ describe('Board 요구사항 테스트', () => {
         }).not.toThrow();
     });
 
+
+    //10번
     test('작성된 Article 목록을 조회 할 수 있어야 한다.', () => {
         const noticeBoard = new Board('공지사항');
         mySite.addBoard(noticeBoard);
@@ -167,6 +277,8 @@ describe('Board 요구사항 테스트', () => {
     });
 });
 
+
+
 describe('Article 요구사항 테스트', () => {
     /**
      * @type {Site}
@@ -179,10 +291,10 @@ describe('Article 요구사항 테스트', () => {
         const noticeBoard = new Board('공지사항');
         mySite.addBoard(noticeBoard);
     });
-
+    //11번
     test('Article은 subject, content, author 3개의 데이터를 포함해야 하며 null 또는 빈 문자열("")은 허용하지 않는다.', () => {
         const noticeBoard = mySite.findBoardByName('공지사항');
-
+        
         expect(() => {
             const article = new Article({
                 subject: '첫번째 공지사항입니다.',
@@ -201,7 +313,7 @@ describe('Article 요구사항 테스트', () => {
             noticeBoard.publish(article2);
         }).toThrow();
     });
-
+    //12번
     test('Board에 추가된 Article만 사용 가능한 것으로 간주하며 사용 불가능한 Article에는 Comment를 추가할 수 없다.', () => {
         const noticeBoard = mySite.findBoardByName('공지사항');
 
@@ -217,7 +329,7 @@ describe('Article 요구사항 테스트', () => {
             content: '댓글을 달 수 없어야 합니다',
             author: '강승현',
         });
-
+       
         expect(() => {
             const comment = new Comment({
                 content: '넵!',
@@ -234,7 +346,7 @@ describe('Article 요구사항 테스트', () => {
             draftArticle.reply(comment);
         }).toThrow();
     });
-
+    //13번
     test('Article에 Comment를 추가할 때 Comment에 작성 일자가 들어가야 한다.', () => {
         const noticeBoard = mySite.findBoardByName('공지사항');
 
@@ -254,7 +366,7 @@ describe('Article 요구사항 테스트', () => {
         // createdDate가 저장되는 형식은 ISO 8601을 따른다.
         expect(/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/.test(comment.createdDate)).toBe(true);
     });
-
+    //14번
     test('Comment는 n개 이상 추가 할 수 있다.', () => {
         const noticeBoard = mySite.findBoardByName('공지사항');
 
@@ -279,7 +391,7 @@ describe('Article 요구사항 테스트', () => {
             article.reply(comment2);
         }).not.toThrow();
     });
-
+    //15번
     test('작성된 Comment 목록을 조회 할 수 있어야 한다.', () => {
         const noticeBoard = mySite.findBoardByName('공지사항');
 
@@ -324,7 +436,7 @@ describe('Comment 요구사항 테스트', () => {
         });
         noticeBoard.publish(article);
     });
-
+    //16번
     test('Comment는 content, author 2개의 데이터를 포함해야 하며 null 또는 빈 문자열("")은 허용하지 않는다.', () => {
         const noticeBoard = mySite.findBoardByName('공지사항');
         const [article] = noticeBoard.getAllArticles();
